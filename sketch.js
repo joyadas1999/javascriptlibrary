d8eb02d79ef2380ccfc5ef0a28fd6dd12dfc1207
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-05-15 00:06:01 -0400

    updated

--- sketch.js
+++ sketch.js
@@ -1,12 +1,3 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-
-
     new Glide('.glide').mount();
-
-    var rellax = new Rellax('.rellax');
-
-    const el = document.querySelector('img');
-const observer = lozad(); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
-observer.observe();
-console.log("lazy loaded~");
 });
